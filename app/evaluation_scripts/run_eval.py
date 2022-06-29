1f6c0e91e3aeb194235bd31573a4b2f1d7884c2e
kkawamu1 <58097201+kkawamu1@users.noreply.github.com>
2022-06-29 15:22:34 +0900

    Update run_eval.py

--- app/evaluation_scripts/run_eval.py
+++ app/evaluation_scripts/run_eval.py
@@ -1,3 +1,20 @@
+# Copyright 2022 Ken Kawamura
+# Copyright BigScience, The HuggingFace Team and The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file has been modifed from the original version on https://github.com/bigscience-workshop/t-zero/blob/master/evaluation/run_eval.py
+
 import torch
 from accelerate import Accelerator
 from transformers import (AutoModelForCausalLM, AutoModelForSeq2SeqLM,
